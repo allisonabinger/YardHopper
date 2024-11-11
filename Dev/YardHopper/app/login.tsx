4eddb31f9bd1240d7f50f24a2665e7213b68c803
marktipton <marktipton95@gmail.com>
2024-11-11 15:26:31 -0600

    added pressable to navigate to tabs

--- Dev/YardHopper/app/login.tsx
+++ Dev/YardHopper/app/login.tsx
@@ -1,13 +1,18 @@
-import { Link } from 'expo-router';
-import { View, Text } from 'react-native'
+import { Link, useRouter } from 'expo-router';
+import { View, Text, Pressable } from 'react-native'
 
 export default function Page() {
+  const router = useRouter();
   return (
     <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Login</Text>
       <Link href="/register" replace>
         <Text>Create a new account</Text>
       </Link>
+
+      <Pressable onPress={() => {router.push("/(tabs)/")}}>
+        <Text>Sign In</Text>
+      </Pressable>
     </View>
   );
 
