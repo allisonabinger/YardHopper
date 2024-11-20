61d3e30fa65c4113d2af17f6a363007eafd61545
marktipton <marktipton95@gmail.com>
2024-11-20 15:13:07 -0600

    added auth logout to logout component

--- Dev/YardHopper/components/LogoutComponent.tsx
+++ Dev/YardHopper/components/LogoutComponent.tsx
@@ -1,16 +1,24 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Pressable } from "react-native";
 import { useRouter } from "expo-router";
+import { useAuth } from "@/components/AuthProvider";
+
 
 export default function LogoutComponent() {
   const router = useRouter();
+  const { logout } = useAuth();
 
-  function logout(){
-    router.replace("/login")
+  async function handleLogout(){
+    try {
+      await logout();
+      router.replace("/login");
+    } catch (error) {
+      console.error("Logout error: ", error);
+    }
   }
 
   return (
-    <Pressable onPress={logout}>
+    <Pressable onPress={handleLogout}>
       <Ionicons name="log-out-outline" size={24} style={{ marginRight: 16 }} />
     </Pressable>
   );
