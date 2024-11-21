89130a0d8d4696d64f7400f46aae7ff4cfdc0395
Chris Gillis <chris85gillis@gmail.com>
2024-11-21 15:03:53 -0600

    Have app connected, worked on polishing listing-details page.

--- Dev/Front-End/app-draft/app/_layout.tsx
+++ Dev/Front-End/app-draft/app/_layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout() {
           <Stack.Screen name="login" options={{ headerShown: false }} />
           <Stack.Screen name="register" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="add-listing-details" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
       </ThemeProvider>
