f17e00d05e84e241bae8cc999c12ec63d3fba5c9
Allison <allisonabinger@gmail.com>
2024-11-25 16:51:28 -0600

    A - 11/25 04:51 PM - Fixed spelling issue for import, updateListing commented out.

--- Dev/Back-End/server/src/routes/listingsRoutes.ts
+++ Dev/Back-End/server/src/routes/listingsRoutes.ts
@@ -1,21 +1,32 @@
 // routes for listing management
 import express from "express";
 import { Request, Response } from "express";
+import multer from "multer";
 import { createListing, fetchListings } from "../controllers/listingsController";
 // import { authenticate } from "../middlewares/authMiddleware"
 
 const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get("/", (req: Request, res: Response) => {
-    fetchListings(req, res);
-  });
-  
-  router.post("/", (req: Request, res: Response) => {
-    createListing(req, res);
-  });
-  
+  fetchListings(req, res);
+});
+
+router.post("/", (req: Request, res: Response) => {
+  createListing(req, res);
+});
+
+// router.put(
+//   "/:postId",
+//   upload.single("image"),
+//   (req: Request, res: Response) => {
+//     updateListing(req, res);
+//   }
+// );
+
 // router.put("/listing/:listingId", updateListing);
 // router.put("/listing/:listingId/images", addListingImage)
 // router.delete("/listing/:listingId", removeListing);
 
-export default router
+export default router;
