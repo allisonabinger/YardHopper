7af3b5d1a18c9035117c820b024bb2d19ba18bf1
Chris Gillis <chris85gillis@gmail.com>
2024-11-27 16:12:32 -0600

    fixing merge conflict

--- Dev/Front-End/app-draft/app/_layout.tsx
+++ Dev/Front-End/app-draft/app/_layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout() {
             <Stack.Screen name="register" options={{ headerShown: false }} />
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="add-listing-details" options={{ headerShown: false }} />
-            <Stack.Screen name="add-listing-details-2" options={{ headerShown: false }} />
+            <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
             <Stack.Screen name="+not-found" options={{ headerShown: false }} />
             <Stack.Screen name="listing/[id]" />
           </Stack>
