c5e5b2225ecd73457746314ab1253b59a2ecf001
Allison <allisonabinger@gmail.com>
2024-11-30 12:04:31 -0600

    A - 11/30 12:02 PM - Added async persistence, updated environment to import .env and export as ENV, user is successfully logged in and remains logged in with async storage. Does NOT know that user is logged in when opening app and navigation needs to be adjusted.

--- Dev/Front-End/app-draft/firebaseConfig.ts
+++ Dev/Front-End/app-draft/firebaseConfig.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth } from "firebase/auth";
-import * as dotenv from "dotenv"
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { ENV } from "./environment";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,15 +12,19 @@ import * as dotenv from "dotenv"
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: ENV.FIREBASE_MESSAGING_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 // Initialize Auth
-export const auth = initializeAuth(app);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+  
