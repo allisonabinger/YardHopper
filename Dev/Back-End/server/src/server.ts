3ca5b7fa71ebdf22aad3f6ceab5ea0ea34e425ba
Allison <allisonabinger@gmail.com>
2024-11-30 15:48:07 -0600

    A - 11/30 03:48 PM - Added default port of 4000

--- Dev/Back-End/server/src/server.ts
+++ Dev/Back-End/server/src/server.ts
@@ -2,7 +2,7 @@
 import app from "./app";
 import { ENV } from "./config/environment";
 
-const PORT = ENV.PORT;
+const PORT = ENV.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
