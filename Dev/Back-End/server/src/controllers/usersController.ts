0223b98ab0b634aae7416aa646d8b3d89e0b798b
Allison <allisonabinger@gmail.com>
2024-12-04 13:54:59 -0600

    A - 12/04 01:54 PM - setting up call and response from fetchUserProfile to getUserProfile

--- Dev/Back-End/server/src/controllers/usersController.ts
+++ Dev/Back-End/server/src/controllers/usersController.ts
@@ -1,27 +1,47 @@
 // Server actions for user management
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { createHash } from "crypto";
+import { getUserProfile } from "services/userService";
 
 export const hashUid = (uid: string): string => {
   return createHash("sha256").update(uid).digest("hex");
 };
 
-/**
- * Endpoint to fetch hashed userId for a given uid
- * This can be used for testing or validating hash generation.
- */
-export const getUserId = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { uid } = req.body; // Ensure UID is passed in the request body
-    if (!uid) {
-      res.status(400).json({ error: "Missing required field: uid" });
-      return;
-    }
+// export const createUser = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+//     try {
+//         const 
+//     }
+// }
 
-    const hashedUserId = hashUid(uid);
-    res.status(200).json({ userId: hashedUserId });
-  } catch (error) {
-    console.error("Error hashing UID:", error);
-    res.status(500).json({ error: "Failed to generate hashed userId" });
-  }
-};
+export const fetchUserProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {hashUid} = req.user;
+        console.log(`fetchUserProfile - hashuid: ${hashUid}`)
+        const userProfile = await getUserProfile(hashUid);
+
+        console.log(`fetchUserProfile - userProfile: ${userProfile}`)
+        if (!userProfile) {
+            return next({ status: 404, message: "User profile not found" });
+        }
+        res.status(200).json(userProfile);
+    } catch (err) {
+        console.log(`Error occured in fetchUserProfile: ${err}`)
+        next(err)
+    }
+}
+
+// export const getUserId = async (req: Request, res: Response): Promise<void> => {
+//   try {
+//     const { uid } = req.body;
+//     if (!uid) {
+//       res.status(400).json({ error: "Missing required field: uid" });
+//       return;
+//     }
+
+//     const hashedUserId = hashUid(uid);
+//     res.status(200).json({ userId: hashedUserId });
+//   } catch (error) {
+//     console.error("Error hashing UID:", error);
+//     res.status(500).json({ error: "Failed to generate hashed userId" });
+//   }
+// };
