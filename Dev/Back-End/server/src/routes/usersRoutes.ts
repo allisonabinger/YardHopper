9f0604a28ad53eda031c558bee9b36cb9f299535
Allison <allisonabinger@gmail.com>
2024-12-04 15:33:11 -0600

    a - 12/04 03:32 PM - configured user routes to use middleware, index added userroutes

--- Dev/Back-End/server/src/routes/usersRoutes.ts
+++ Dev/Back-End/server/src/routes/usersRoutes.ts
@@ -1,1 +1,30 @@
-// routes for user management
+// usersRouters - routes for user management
+import { fetchUserProfile } from "../controllers/usersController";
+import express from "express";
+import { Request, Response } from "express";
+import { authenticateUser } from "../middlewares/authMiddleware";
+
+const router = express.Router();
+
+// create user
+// router.post("/", authenticateUser, createUser);
+
+router.get("/me", authenticateUser, fetchUserProfile)
+
+// // update user
+// router.put("/:userId", (req: Request, res: Response) => {
+//     updateUser(req, res);
+// });
+
+// // get all listings saved by a user
+// router.get("/:userId/saved", (req: Request, res: Response) => {
+//       createListing(req, res);
+// });
+
+// // add a new listing to a user's saved listings
+// router.post("/:userId/saved")
+
+// // get all listings made my a user
+// router.get("/:userId/listings")
+
+export default router;
