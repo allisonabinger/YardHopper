4b00c4e5f7ad0cee698d6759a6ac8fd92e01f662
Allison <allisonabinger@gmail.com>
2024-12-04 15:33:26 -0600

    a - 12/04 03:33 PM - pulls user information if available

--- Dev/Back-End/server/src/services/userService.ts
+++ Dev/Back-End/server/src/services/userService.ts
@@ -1,1 +1,37 @@
-// Service layer for Firestore (user)
+// Service layer for user management
+import { signInWithCustomToken } from "firebase/auth";
+import { db, auth } from "../config/firebase";
+// import { generateCoordinates, generateGeo } from "./geolocateService";
+
+export const getUserProfile = async (
+    hashUid: string,
+) => {
+    try {
+          const userRef = db.collection("users").doc(hashUid);
+          const userDoc = await userRef.get();
+
+          if (!userDoc.exists) {
+            return null;
+      }
+
+          const data = userDoc.data();
+
+          if (!data) {
+            return null;
+          }
+          return {
+            first: data.first,
+            last: data.last,
+            email: data.email,
+            street: data.street ?? undefined,
+            city: data.city ?? undefined,
+            state: data.state ?? undefined,
+            zipcode: data.zipcode,
+            createdAt: data.createdAt,
+          };
+
+    } catch (err) {
+          console.error("Error finding listing in Firestore: ", err);
+          throw err;
+    }
+};
