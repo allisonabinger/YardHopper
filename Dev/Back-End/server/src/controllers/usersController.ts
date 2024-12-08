c25b24c403b65ada7c94d4d79a6046f25a81fd07
Allison <allisonabinger@gmail.com>
2024-12-07 19:01:55 -0600

    A - 12/07 07:01 PM - moved hashUid into authmiddleware

--- Dev/Back-End/server/src/controllers/usersController.ts
+++ Dev/Back-End/server/src/controllers/usersController.ts
@@ -1,6 +1,5 @@
 // Server actions for user management
 import { NextFunction, Request, Response } from "express";
-import { createHash } from "crypto";
 import {
     getSavedListings,
     getUserListings,
@@ -11,17 +10,6 @@ import {
     unsaveListingToUser,
     updateUserProfile,
 } from "../services/userService";
-import { User } from "../models/userModel";
-
-export const hashUid = (uid: string): string => {
-    return createHash("sha256").update(uid).digest("hex");
-};
-
-// export const createUser = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-//     try {
-//         const
-//     }
-// }
 
 export const fetchUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -195,18 +183,3 @@ export const unsaveListing = async (req: Request, res: Response, next: NextFunct
         next(err);
     }
 };
-// export const getUserId = async (req: Request, res: Response): Promise<void> => {
-//   try {
-//     const { uid } = req.body;
-//     if (!uid) {
-//       res.status(400).json({ error: "Missing required field: uid" });
-//       return;
-//     }
-
-//     const hashedUserId = hashUid(uid);
-//     res.status(200).json({ userId: hashedUserId });
-//   } catch (error) {
-//     console.error("Error hashing UID:", error);
-//     res.status(500).json({ error: "Failed to generate hashed userId" });
-//   }
-// };
